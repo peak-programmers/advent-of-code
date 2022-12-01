8c846aa06ac6a5d6e86884818a9c08d3bbf9b357
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-01 21:26:39 +0000

    Get max value of summed inventories

--- 2022/jack/day01/src/calorie-counter.spec.ts
+++ 2022/jack/day01/src/calorie-counter.spec.ts
@@ -1,14 +1,15 @@
 import CalorieCounter from './calorie-counter';
 
 describe('CalorieCounter()', () => {
-  it.each([
-    { foodItems: [], expected: 0 },
-    { foodItems: [1000, 2000, 3000], expected: 6000 },
-    { foodItems: [4000], expected: 4000 },
-  ])(
-    'should return $expected as the total of the sum of the elements of $foodItems',
-    ({ foodItems, expected }) => {
-      expect(CalorieCounter.getTotalCalories(foodItems)).toBe(expected);
-    }
-  );
+  it('should return the max value of an array of elfInventories', () => {
+    const elfInventories = [
+      [1000, 2000, 3000],
+      [4000],
+      [5000, 6000],
+      [7000, 8000, 9000],
+      [10000],
+    ];
+
+    expect(CalorieCounter.getMaxInventoryValue(elfInventories)).toBe(24000);
+  });
 });
--- 2022/jack/day01/src/calorie-counter.ts
+++ 2022/jack/day01/src/calorie-counter.ts
@@ -1,5 +1,16 @@
 export default class CalorieCounter {
-  public static getTotalCalories(foodItems: number[]): number {
-    return foodItems.reduce((a, b) => a + b, 0);
+  public static getMaxInventoryValue(elfInventories: number[][]): number {
+    const sumInventoriesReducer = (
+      acc: number[],
+      element: number[]
+    ): number[] => {
+      return [...acc, this.sumCalories(element)];
+    };
+
+    return Math.max(...elfInventories.reduce(sumInventoriesReducer, []));
+  }
+
+  private static sumCalories(foodItems: number[]): number {
+    return foodItems.reduce((a: number, b: number) => a + b, 0);
   }
 }
