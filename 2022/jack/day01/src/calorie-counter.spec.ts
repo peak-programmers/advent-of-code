cb2f5b0f8f685944c9b8f8058a3a3a0f924db327
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-01 21:06:12 +0000

    A few more test cases (plus updated readme)

--- 2022/jack/day01/src/calorie-counter.spec.ts
+++ 2022/jack/day01/src/calorie-counter.spec.ts
@@ -1,10 +1,14 @@
 import CalorieCounter from './calorie-counter';
 
 describe('CalorieCounter()', () => {
-  it('should count the total calories in the food items an elf is carrying', () => {
-    const result = CalorieCounter.getTotalCalories([1000, 2000, 3000]);
-    const expected = 6000;
-
-    expect(result).toBe(expected);
-  });
+  it.each([
+    { foodItems: [], expected: 0 },
+    { foodItems: [1000, 2000, 3000], expected: 6000 },
+    { foodItems: [4000], expected: 4000 },
+  ])(
+    'should return $expected as the total of the sum of the elements of $foodItems',
+    ({ foodItems, expected }) => {
+      expect(CalorieCounter.getTotalCalories(foodItems)).toBe(expected);
+    }
+  );
 });
