8d9a9eb57a813b071423cac1167fa0941d475b3d
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-02 13:14:47 +0000

    Reducer refactor

--- 2022/jack/day01/src/calorie-counter.ts
+++ 2022/jack/day01/src/calorie-counter.ts
@@ -1,31 +1,24 @@
 export default class CalorieCounter {
   public static calculateMaxInventoryValue(elfInventories: number[][]): number {
-    const sumInventoriesReducer = (
-      acc: number[],
-      element: number[]
-    ): number[] => {
-      return [...acc, this.sumCalories(element)];
-    };
-
-    return Math.max(...elfInventories.reduce(sumInventoriesReducer, []));
+    return Math.max(...elfInventories.reduce(this.sumInventoriesReducer, []));
   }
 
   public static calculateSumOfTopThree(elfInventories: number[][]): number {
-    const sumInventoriesReducer = (
-      acc: number[],
-      element: number[]
-    ): number[] => {
-      return [...acc, this.sumCalories(element)];
-    };
-
     const sorted = elfInventories
-      .reduce(sumInventoriesReducer, [])
+      .reduce(this.sumInventoriesReducer, [])
       .sort((a, b) => b - a);
 
     return this.sumCalories([sorted[0], sorted[1], sorted[2]]);
   }
 
-  private static sumCalories(foodItems: number[]): number {
-    return foodItems.reduce((a: number, b: number) => a + b, 0);
+  private static sumInventoriesReducer(
+    acc: number[],
+    element: number[]
+  ): number[] {
+    return [...acc, this.sumCalories(element)];
+  }
+
+  private static sumCalories(inventory: number[]): number {
+    return inventory.reduce((a: number, b: number) => a + b, 0);
   }
 }
