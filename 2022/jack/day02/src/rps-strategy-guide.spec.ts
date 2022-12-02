fe97621f2906eb5ee0aa9b8c404cdb648bce9614
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-02 22:23:03 +0000

    Rock v Paper

--- 2022/jack/day02/src/rps-strategy-guide.spec.ts
+++ 2022/jack/day02/src/rps-strategy-guide.spec.ts
@@ -1,10 +1,10 @@
 import RpsStrategyGuide from './rps-strategy-guide';
 
 describe('RpsStrategyGuide()', () => {
-  it.each([{ input: 'A X', result: 4 }])(
-    'should return the score $result for game $input',
-    ({ input, result }) => {
-      expect(RpsStrategyGuide.calculateScore(input)).toBe(result);
-    }
-  );
+  it.each([
+    { input: 'A X', result: 4 },
+    { input: 'A Y', result: 8 },
+  ])('should return the score $result for game $input', ({ input, result }) => {
+    expect(RpsStrategyGuide.calculateScore(input)).toBe(result);
+  });
 });
--- 2022/jack/day02/src/rps-strategy-guide.ts
+++ 2022/jack/day02/src/rps-strategy-guide.ts
@@ -3,6 +3,7 @@ type GameScores = { [key: string]: number };
 export default class RpsStrategyGuide {
   private static readonly gameScores: GameScores = {
     ['A X']: 4,
+    ['A Y']: 8,
   };
 
   public static calculateScore(game: string) {
