78387f780f9005ca8a778002b59c8d0945254757
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-02 13:22:09 +0000

    revert

--- 2022/jack/day01/src/calorie-counter.ts
+++ 2022/jack/day01/src/calorie-counter.ts
@@ -1,11 +1,25 @@
 export default class CalorieCounter {
   public static calculateMaxInventoryValue(elfInventories: number[][]): number {
-    return Math.max(...elfInventories.reduce(this.sumInventoriesReducer, []));
+    const sumInventoriesReducer = (
+      acc: number[],
+      element: number[]
+    ): number[] => {
+      return [...acc, this.sumCalories(element)];
+    };
+
+    return Math.max(...elfInventories.reduce(sumInventoriesReducer, []));
   }
 
   public static calculateSumOfTopThree(elfInventories: number[][]): number {
+    const sumInventoriesReducer = (
+      acc: number[],
+      element: number[]
+    ): number[] => {
+      return [...acc, this.sumCalories(element)];
+    };
+
     const sorted = elfInventories
-      .reduce(this.sumInventoriesReducer, [])
+      .reduce(sumInventoriesReducer, [])
       .sort((a, b) => b - a);
 
     return this.sumCalories([sorted[0], sorted[1], sorted[2]]);
@@ -18,7 +32,7 @@ export default class CalorieCounter {
     return [...acc, this.sumCalories(element)];
   }
 
-  private static sumCalories(inventory: number[]): number {
-    return inventory.reduce((a: number, b: number) => a + b, 0);
+  private static sumCalories(foodItems: number[]): number {
+    return foodItems.reduce((a: number, b: number) => a + b, 0);
   }
 }
