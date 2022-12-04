c7e3cdf93a02da86b5e2d06a27b39a3bd2bae60c
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-04 19:37:39 +0000

    Finish part 2

--- 2022/jack/day04/src/main.ts
+++ 2022/jack/day04/src/main.ts
@@ -7,5 +7,8 @@ const assignmentPairs: AssignmentPair[] =
 
 const part1Result: number =
   CampCleanupOrganiser.calculateTotalFullSectionDuplication(assignmentPairs);
+const part2Result: number =
+  CampCleanupOrganiser.calculateTotalPartialSectionDuplication(assignmentPairs);
 
 console.log('Part 1 result: ', part1Result);
+console.log('Part 2 result: ', part2Result);
