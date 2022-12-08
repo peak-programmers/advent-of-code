d9392fbce89bf099e4abd93b9fb83503c2800841
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-08 13:37:01 +0000

    File processor refactor

--- 2022/jack/day07/src/file-processor.ts
+++ 2022/jack/day07/src/file-processor.ts
@@ -7,36 +7,70 @@ export default class FileProcessor {
   public static processInput(inputPath: string): IDirectory {
     const rawCommands = fs.readFileSync(inputPath).toString().split('\n');
     const fileSystem = this.mapFileSystemFromCommands(rawCommands);
-    console.log('hello', fileSystem);
 
     return fileSystem;
   }
 
-  private static mapFileSystemFromCommands(rawCommands: string[]): IDirectory {
+  private static mapFileSystemFromCommands(
+    rawTerminalOutput: string[]
+  ): IDirectory {
     let currentDir: IDirectory = new Directory('/', new NullDirectory());
-    rawCommands.map((command) => {
-      if (command.includes('$ cd')) {
-        currentDir = currentDir.cd(command.split(' ')[2]);
-      } else if (command.includes('$ ls')) {
-      } else if (!command.includes('$')) {
-        if (command.includes('dir')) {
-          const directoryName = command.split(' ')[1];
-          if (
-            !currentDir
-              .ls()
-              .directories.find(
-                (directory) => directory.getName() === directoryName
-              )
-          )
-            currentDir.addChildDirectory(directoryName);
-        } else {
-          const [size, name] = command.split(' ');
-          if (!currentDir.ls().files.find((file) => file.name === name))
-            currentDir.addFile({ name, size: parseInt(size) });
-        }
+    rawTerminalOutput.map((terminalOutput) => {
+      currentDir = this.cdIfCdCommand(terminalOutput, currentDir);
+
+      if (!terminalOutput.includes('$')) {
+        this.addDirectoryIfDirectory(terminalOutput, currentDir);
+        this.addFileOtherwise(terminalOutput, currentDir);
       }
     });
 
     return currentDir.cd('/');
+  }
+
+  private static cdIfCdCommand(
+    command: string,
+    currentDir: IDirectory
+  ): IDirectory {
+    return command.includes('$ cd')
+      ? currentDir.cd(command.split(' ')[2])
+      : currentDir;
+  }
+
+  private static addDirectoryIfDirectory(
+    terminalOutput: string,
+    currentDir: IDirectory
+  ): void {
+    if (terminalOutput.includes('dir')) {
+      const directoryName = terminalOutput.split(' ')[1];
+      if (this.childDirectoryNotInParent(directoryName, currentDir))
+        currentDir.addChildDirectory(directoryName);
+    }
+  }
+
+  private static childDirectoryNotInParent(
+    directoryName: string,
+    currentDir: IDirectory
+  ): boolean {
+    return !currentDir
+      .ls()
+      .directories.find((directory) => directory.getName() === directoryName);
+  }
+
+  private static addFileOtherwise(
+    terminalOutput: string,
+    currentDir: IDirectory
+  ): void {
+    if (!terminalOutput.includes('dir')) {
+      const [size, name] = terminalOutput.split(' ');
+      if (this.fileNotInDirectory(name, currentDir))
+        currentDir.addFile({ name, size: parseInt(size) });
+    }
+  }
+
+  private static fileNotInDirectory(
+    name: string,
+    currentDir: IDirectory
+  ): boolean {
+    return !currentDir.ls().files.find((file) => file.name === name);
   }
 }
