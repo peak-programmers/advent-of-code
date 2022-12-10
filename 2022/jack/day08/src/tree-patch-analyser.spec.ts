d9305e133bce648a9afd5bd1fd5f5031a0848442
Jack <jackrimmer2996@hotmail.co.uk>
2022-12-10 17:37:20 +0000

    Part 2 test passes

--- 2022/jack/day08/src/tree-patch-analyser.spec.ts
+++ 2022/jack/day08/src/tree-patch-analyser.spec.ts
@@ -12,4 +12,16 @@ describe('TreePatchAnalyser()', () => {
 
     expect(TreePatchAnalyser.visibleTreesCount(treePatch)).toBe(21);
   });
+
+  it('should return the highest scenic score in a patch', () => {
+    const treePatch = [
+      [3, 0, 3, 7, 3],
+      [2, 5, 5, 1, 2],
+      [6, 5, 3, 3, 2],
+      [3, 3, 5, 4, 9],
+      [3, 5, 3, 9, 0],
+    ];
+
+    expect(TreePatchAnalyser.calculateMaxScenicScore(treePatch)).toBe(8);
+  });
 });
--- 2022/jack/day08/src/tree-patch-analyser.ts
+++ 2022/jack/day08/src/tree-patch-analyser.ts
@@ -4,6 +4,67 @@ type GridIndex = {
 };
 
 export default class TreePatchAnalyser {
+  public static calculateMaxScenicScore(treePatch: number[][]): number {
+    const scenicScoreMap = this.buildScenicScoreMap(treePatch);
+
+    return scenicScoreMap.reduce((acc: number, scoreRow: number[]) => {
+      return Math.max(acc, Math.max(...scoreRow));
+    }, 0);
+  }
+
+  private static buildScenicScoreMap(treePatch: number[][]): number[][] {
+    return treePatch.map((treeRow: number[], row: number) =>
+      treeRow.map((tree: number, col: number) => {
+        const leftOfTree = treeRow.filter((_, index) => index < col).reverse();
+        const leftViewingDistance = this.calculateViewingDistance(
+          leftOfTree,
+          tree
+        );
+
+        const rightOfTree = treeRow.filter((_, index) => index > col);
+        const rightViewingDistance = this.calculateViewingDistance(
+          rightOfTree,
+          tree
+        );
+
+        const treeCol = this.transposeRow(col, treePatch);
+
+        const aboveTree = treeCol.filter((_, index) => index < row).reverse();
+        const aboveViewingDistance = this.calculateViewingDistance(
+          aboveTree,
+          tree
+        );
+
+        const belowTree = treeCol.filter((_, index) => index > row);
+        const belowViewingDistance = this.calculateViewingDistance(
+          belowTree,
+          tree
+        );
+
+        return (
+          leftViewingDistance *
+          rightViewingDistance *
+          aboveViewingDistance *
+          belowViewingDistance
+        );
+      })
+    );
+  }
+
+  private static calculateViewingDistance(
+    treeSubsection: number[],
+    tree: number
+  ) {
+    if (treeSubsection.length === 0) return 0;
+
+    const firstBlockingIndex = treeSubsection.findIndex(
+      (adjacentTree) => adjacentTree >= tree
+    );
+    return firstBlockingIndex === -1
+      ? treeSubsection.length
+      : firstBlockingIndex + 1;
+  }
+
   public static visibleTreesCount(treePatch: number[][]): number {
     return this.countVisible(this.buildVisibiltyMap(treePatch));
   }
